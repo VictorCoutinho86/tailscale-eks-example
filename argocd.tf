69b350ba44c4354469c224b7d5ba23bcc9216e72
Victor <victor.melo@gazeus.com>
2026-07-18 17:36:08 -0300

    feat: forward kubecost cur values

--- argocd.tf
+++ argocd.tf
@@ -40,18 +40,23 @@ resource "helm_release" "argocd_root_application" {
   timeout = 300
 
   values = [yamlencode({
-    repoURL                = "https://github.com/VictorCoutinho86/tailscale-eks-example.git"
-    targetRevision         = "master"
-    clusterName            = module.eks.cluster_name
-    awsRegion              = var.aws_region
-    vpcId                  = module.vpc.vpc_id
-    domain                 = trimsuffix(var.route53_domain_name, ".")
-    certificateArn         = aws_acm_certificate_validation.platform.certificate_arn
-    karpenterQueueName     = module.karpenter.queue_name
-    karpenterNodeRoleName  = module.karpenter.node_iam_role_name
-    sparkWorkloadNamespace = var.spark_workload_namespace
-    airflowLogsBucket      = var.airflow_logs_bucket
-    argocdAdminPassword    = var.argocd_admin_password
+    repoURL                          = "https://github.com/VictorCoutinho86/tailscale-eks-example.git"
+    targetRevision                   = "master"
+    clusterName                      = module.eks.cluster_name
+    awsRegion                        = var.aws_region
+    vpcId                            = module.vpc.vpc_id
+    domain                           = trimsuffix(var.route53_domain_name, ".")
+    certificateArn                   = aws_acm_certificate_validation.platform.certificate_arn
+    karpenterQueueName               = module.karpenter.queue_name
+    karpenterNodeRoleName            = module.karpenter.node_iam_role_name
+    sparkWorkloadNamespace           = var.spark_workload_namespace
+    airflowLogsBucket                = var.airflow_logs_bucket
+    kubecostAthenaAccountId          = data.aws_caller_identity.current.account_id
+    kubecostAthenaDatabase           = var.kubecost_athena_database
+    kubecostAthenaTable              = var.kubecost_athena_table
+    kubecostAthenaQueryResultsBucket = var.kubecost_athena_query_results_bucket
+    kubecostAthenaWorkgroup          = var.kubecost_athena_workgroup
+    argocdAdminPassword              = var.argocd_admin_password
   })]
 
   depends_on = [helm_release.argocd]
